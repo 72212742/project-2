b353bf1fc80676ffe3293952b4d812c8681686f2
72212742 <154432540+72212742@users.noreply.github.com>
2023-12-20 18:47:56 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,20 @@
+import random
+
+def number_guessing_game():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("Guess the number (1-100): "))
+        attempts += 1
+
+        if guess == secret_number:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+        elif guess < secret_number:
+            print("Too low. Try again.")
+        else:
+            print("Too high. Try again.")
+
+# Example usage:
+number_guessing_game()
